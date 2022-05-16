8b379fa0ec8cb188f931733f888e7d51df60b1b5
nng <itsnathanng@gmail.com>
2022-05-16 16:53:52 -0700

    Created a basic GUI with QLabels, QLineEdit, and QPushButton to add recipes and their serving size

--- grocery_generator.py
+++ grocery_generator.py
@@ -14,24 +14,86 @@
 ## Export Recipes using JSON or CSV
 
 ## Ingredients need name, unit, and quantity    
+
+## Have it such that I can add text into text fields and click a button and it will print out to terminal success
+
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QLineEdit
 
-def window():
-   app = QApplication(sys.argv) # What is the purpose of this line?
-   w = QWidget()
-   b = QLabel(w)
-   c = QLineEdit(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   c.move(100, 0)
-   w.setWindowTitle("Grocery List Generator")
-   w.show()
-   sys.exit(app.exec_())
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Grocerly List App")
+
+        self.recipe_name_label = QLabel("Recipe Name")
+        self.recipe_name_label.setFixedHeight(25)
+        self.recipe_name_label.setFixedWidth(100)
+
+        self.recipe_name_input = QLineEdit()
+        self.recipe_name_input.setFixedHeight(30)
+        self.recipe_name_input.setFixedWidth(100)
+
+        self.recipe_servings_label = QLabel("Number of servings")
+        self.recipe_servings_label.setFixedHeight(25)
+        self.recipe_servings_label.setFixedWidth(150)
+
+        self.recipe_servings_input = QLineEdit()
+        self.recipe_servings_input.setFixedHeight(30)
+        self.recipe_servings_input.setFixedWidth(100)
+
+        self.submit_button = QPushButton("Submit")
+        self.submit_button.setFixedHeight(30)
+        self.submit_button.setFixedWidth(65)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.recipe_name_label)
+        layout.addWidget(self.recipe_name_input)
+        layout.addWidget(self.recipe_servings_label)
+        layout.addWidget(self.recipe_servings_input)
+        layout.addWidget(self.submit_button)
+        
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+        self.submit_button.clicked.connect(self.submit_pushed)
+
+    def submit_pushed(self):
+        print("submit button pushed")
+        name = self.recipe_name_input.text()
+        servings = self.recipe_servings_input.text()
+        valid_entries = True
+
+        if len(name) == 0:
+            print("Recipe name field is empty. Please enter value.")
+            valid_entries = False
+        
+        if len(servings) == 0:
+            print("Servings field is empty. Please enter value")
+            valid_entries = False
+        
+        if not servings.isnumeric():
+            print("Servings value is not a number. Please enter a number.")
+            valid_entries = False
+        
+        if valid_entries:
+            print("Recipe name is: {} and it serves {} people".format(name, servings))
+            self.recipe_name_input.clear()
+            self.recipe_servings_input.clear()
+
+if __name__ == '__main__':
+
+   app = QApplication(sys.argv)
+   window = MainWindow()
+   window.show()
+
+   ## Initiates the event loop 
+   app.exec_()
+
+
+'''
 class Recipe:
     def __init__(self, name, ingredients):
         self.name = name
@@ -49,9 +111,4 @@ class Recipe:
         except ValueError:
             print("{} not in list".format(ingredient))
 
-chicken_rice = Recipe("Chicken and Rice", ["1 lb Chicken", "1 cup Rice"])
-
-if __name__ == '__main__':
-   window()
-
-
+'''
