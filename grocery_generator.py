2d36ec49f5e9eaf6844beb9b28aa2b5c30a18bb4
nng <itsnathanng@gmail.com>
2022-05-25 16:43:58 -0700

    Add slot to recipe master list such that when a recipe is clicked the page displays its name, author, ingredients

--- grocery_generator.py
+++ grocery_generator.py
@@ -1,43 +1,87 @@
 ## TODO
 
 # Create a view area for people to see their recipe after selecting it in the table
-# Remove the ability to edit directly in the table (do not want Spreadsheet style)
 # Want a push button for "Add Recipe"
 # A comboBox with the recipes and an "Edit Recipe" push button
 # Send you an email with your shopping list and meal plan
 # Import Recipes using a JSON or CSV
 # Export Recipes using JSON or CSV
-# Ingredients need name, unit, and quantity    
+# Ingredients need name, unit, and quantity
+# Adding a database will allow users to share recipes with one another    
 
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, \
     QVBoxLayout, QWidget, QLineEdit, QTableWidget, QTableWidgetItem, QComboBox, \
     QListWidget, QHBoxLayout, QVBoxLayout, QTextBrowser
 
+# Ingredient data structure that holds name, units, and quantity of an ingredient
+class Ingredient:
+    def __init__(self, name, unit, quantity):
+        self.name = name
+        self.unit = unit
+        self.quantity = quantity
+
+## Class for storing recipe information ##
+## Recipe Name, Servings, Author Name, Ingredients List, Instructions
+class Recipe:
+    def __init__(self, recipe_name, servings, author):
+        self.name = recipe_name
+        self.servings = servings
+        self.author = author
+        self.ingredients = {}
+        self.instructions = {}
+    
+    def add_ingredient(self, ingredient, unit, quantity):
+        ##TODO add some error checking of inputs
+
+        if ingredient in self.ingredients:
+            self.ingredients[ingredient].unit = unit
+            self.ingredients[ingredient].quantity = quantity
+        else:
+            self.ingredients[ingredient] = Ingredient(ingredient, unit, quantity)
+        
+    
+    def print_ingredients(self):
+        for ingredient in list(self.ingredients.values()):
+            print("{quantity} {unit} of {ingredient}".format(quantity=ingredient.quantity\
+                , unit=ingredient.unit, ingredient=ingredient.name))
+    
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.recipe_master = {}
+        test_recipe = Recipe("Ribs and Cauliflower", 2, "Susan Xie")
+        test_recipe.add_ingredient("Ribs", "lb", "1")
+        test_recipe.add_ingredient("Cauliflower", "bunch", "1")
+        test_recipe.print_ingredients()
+        self.recipe_master.update({test_recipe.name: test_recipe})
+
         ### Widgets ###
-        self.setWindowTitle("Grocerly List App")
+        self.setWindowTitle("Grocery List App")
         self.setFixedSize(700, 500)
 
         self.actionMenu = QComboBox()
         self.actionMenu.addItems(["View Recipe", "Add Recipe", "Edit Recipe"])
 
         self.recipeList = QListWidget()
-        self.recipeList.addItem("Ribs and Cauliflower")
+        self.recipeList.addItems(list(self.recipe_master.keys()))
 
-        self.recipe_label = QLabel("Ribs")
-        self.author_label = QLabel("Nathan")
+        self.recipe_label = QLabel()
+        self.author_label = QLabel()
+
+        self.servings_label = QLabel("Servings: ")
 
         self.ingredient_label = QLabel("Ingredients")
         self.ingredient_list = QListWidget()
-        self.ingredient_list.addItem("Water")
+    
         self.instruction_label = QLabel("Instructions")
         self.instructions = QTextBrowser()
 
-        ### Build Main Layout ###
+        #self.instructions.setSource("./instructions.txt")
+
+        ### Create Left and Right Panel ###
         h_layout = QHBoxLayout()
         v_layout_left = QVBoxLayout()
         v_layout_right = QVBoxLayout()
@@ -45,15 +89,16 @@ class MainWindow(QMainWindow):
         h_layout.addLayout(v_layout_right)
         layout = h_layout
 
-        ### Build Left Panel ##
+        ### Add Widgets to Left Panel ##
         v_layout_left.addWidget(self.actionMenu)
         v_layout_left.addWidget(self.recipeList)
 
-        ## Build Right Panel - View Recipes ##
+        ## Add Widgets to Right Panel - View Recipes ##
         header_bar = QHBoxLayout()
         header_bar.addWidget(self.recipe_label)
         header_bar.addWidget(self.author_label)
         v_layout_right.addLayout(header_bar)
+        v_layout_right.addWidget(self.servings_label)
         v_layout_right.addWidget(self.ingredient_label)
         v_layout_right.addWidget(self.ingredient_list)
         v_layout_right.addWidget(self.instruction_label)
@@ -63,11 +108,22 @@ class MainWindow(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-        ### Initialize Global Variables ###
-        self.recipe_name_master = {}
-
         ### Signals ###
+        self.recipeList.itemPressed.connect(self.recipe_pressed)
         #self.submit_button.clicked.connect(self.submit_pushed)
+    
+    ## Function that handles the event when a recipe is clicked in the recipe list
+    def recipe_pressed(self, item):
+        print("{} was selected".format(item.text()))
+        recipe = self.recipe_master[item.text()]
+        self.recipe_label.setText(recipe.name)
+        self.author_label.setText("By: {}".format(recipe.author))
+        self.servings_label.setText("Servings: {}".format(recipe.servings))
+        ## Add each ingredient to the list
+        for ingredient in list(recipe.ingredients.values()):
+            self.ingredient_list.addItem("{quantity} {unit}(s) of {ingredient}".format(\
+                quantity=ingredient.quantity, unit=ingredient.unit, ingredient=ingredient.name))
+        
     
 if __name__ == '__main__':
 
@@ -77,81 +133,6 @@ if __name__ == '__main__':
    app.exec_()
 
 
-'''
-        self.recipe_name_label = QLabel("Recipe Name")
-        self.recipe_name_label.setFixedHeight(25)
-        self.recipe_name_label.setFixedWidth(100)
 
-        self.recipe_name_input = QLineEdit()
-        self.recipe_name_input.setFixedHeight(30)
-        self.recipe_name_input.setFixedWidth(100)
 
-        self.recipe_servings_label = QLabel("Number of servings")
-        self.recipe_servings_label.setFixedHeight(25)
-        self.recipe_servings_label.setFixedWidth(150)
-
-        self.recipe_servings_input = QLineEdit()
-        self.recipe_servings_input.setFixedHeight(30)
-        self.recipe_servings_input.setFixedWidth(100)
-
-        self.submit_button = QPushButton("Submit")
-        self.submit_button.setFixedHeight(30)
-        self.submit_button.setFixedWidth(65)
-
-        self.recipe_table = QTableWidget(0, 2)
-        self.recipe_table.setHorizontalHeaderLabels(["Recipe", "Servings"])
-        '''
-
-'''
-    def submit_pushed(self):
-        print("submit button pushed")
-        name = self.recipe_name_input.text()
-        servings = self.recipe_servings_input.text()
-        valid_entries = True
-
-        if len(name) == 0:
-            print("Recipe name field is empty. Please enter value.")
-            valid_entries = False
-        
-        if len(servings) == 0:
-            print("Servings field is empty. Please enter value")
-            valid_entries = False
-        
-        if not servings.isnumeric():
-            print("Servings value is not a number. Please enter a number.")
-            valid_entries = False
-        
-        if valid_entries:
-            print("Recipe name is: {} and it serves {} people".format(name, servings))
-
-            self.add_recipe(name, servings)
-            self.recipe_name_input.clear()
-            self.recipe_servings_input.clear()
-
-    def add_recipe(self, name, servings):
-    
-        if self.recipeDoesNotExist(name):
-            recipe_name = QTableWidgetItem(name)
-            recipe_servings = QTableWidgetItem(servings)
-            self.recipe_name_master.update({name: True})
-
-            self.recipe_table.setRowCount(self.recipe_table.rowCount()+1)
-            self.recipe_table.setItem(self.recipe_table.rowCount()-1, 0, recipe_name)
-            self.recipe_table.setItem(self.recipe_table.rowCount()-1, 1, recipe_servings)
-        else:
-            print("You have attempted to add a recipe name that already exists. The recipe was not added.")
-    
-    def recipeDoesNotExist(self, recipe):
-        if recipe in self.recipe_name_master:
-            return False
-        return True
-    
-    
-        layout.addWidget(self.recipe_name_label)
-        layout.addWidget(self.recipe_name_input)
-        layout.addWidget(self.recipe_servings_label)
-        layout.addWidget(self.recipe_servings_input)
-        layout.addWidget(self.submit_button)
-        layout.addWidget(self.recipe_table)
-        '''    
         
